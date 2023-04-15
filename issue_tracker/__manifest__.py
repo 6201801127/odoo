afdd0efb335f3e896e6c91a361db98592a0b5668
ajay.ravidas <ajay.16.1.1997ravidas@gmail.com>
2023-04-15 09:21:37 +0530

    changes in name

--- issue_tracker/__manifest__.py
+++ issue_tracker/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "issue_tracker",
+    'name': "Issue Tracker",
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
@@ -8,7 +8,7 @@
     'description': """
         Long description of module's purpose
     """,
-    'author': "My Company",
+    'author': "Ajay Kumar Ravidas",
     'website': "http://www.yourcompany.com",
     'category': 'Uncategorized',
     'version': '0.1',
